91de2f953134d0bdcbf73a767788db04698f47fe
yt8492 <yt8492@users.noreply.github.com>
2023-05-05 18:26:57 +0000

    deploy: cb545f319c812e963d0aeaf25182f4bf03416d82

--- KotlinWasmPlayground-wasm.uninstantiated.mjs
+++ KotlinWasmPlayground-wasm.uninstantiated.mjs
@@ -17,19 +17,19 @@ export async function instantiate(imports={}, runInitializer=true) {
 
     
     const js_code = {
-        'kotlin.captureStackTrace_-1334956621' : () => new Error().stack,
-        'kotlin.wasm.internal.throwJsError_-385587655' : (message, wasmTypeName, stack) => { 
+        'kotlin.captureStackTrace_-1527000587' : () => new Error().stack,
+        'kotlin.wasm.internal.throwJsError_241251258' : (message, wasmTypeName, stack) => { 
                 const error = new Error();
                 error.message = message;
                 error.name = wasmTypeName;
                 error.stack = stack;
                 throw error;
            },
-        'kotlin.wasm.internal.getJsEmptyString_-164465437' : () => '',
-        'kotlin.wasm.internal.getJsTrue_-1478181067' : () => true,
-        'kotlin.wasm.internal.getJsFalse_1477904862' : () => false,
-        'kotlin.wasm.internal.stringLength_1772554682' : x => x.length,
-        'kotlin.wasm.internal.jsExportStringToWasm_271128330' :  (src, srcOffset, srcLength, dstAddr) => {
+        'kotlin.wasm.internal.getJsEmptyString_-356509403' : () => '',
+        'kotlin.wasm.internal.getJsTrue_-1670225033' : () => true,
+        'kotlin.wasm.internal.getJsFalse_1285860896' : () => false,
+        'kotlin.wasm.internal.stringLength_-1711984102' : x => x.length,
+        'kotlin.wasm.internal.jsExportStringToWasm_-764483699' :  (src, srcOffset, srcLength, dstAddr) => {
                 const mem16 = new Uint16Array(wasmExports.memory.buffer, dstAddr, srcLength);
                 let arrayIndex = 0;
                 let srcIndex = srcOffset;
@@ -40,14 +40,17 @@ export async function instantiate(imports={}, runInitializer=true) {
                 }
             }
         ,
-        'kotlin.wasm.internal.importStringFromWasm_-1434832627' : (address, length, prefix) => {
+        'kotlin.wasm.internal.newJsArray_2038538740' : () => [],
+        'kotlin.wasm.internal.jsArrayPush_1062523760' : (array, element) => { array.push(element); },
+        'kotlin.wasm.internal.importStringFromWasm_-1852552787' : (address, length, prefix) => {
             const mem16 = new Uint16Array(wasmExports.memory.buffer, address, length);
             const str = String.fromCharCode.apply(null, mem16);
             return (prefix == null) ? str : prefix + str;
         }
         ,
-        'kotlin.wasm.internal.externrefToString_871258856' : ref => String(ref),
-        'kotlin.wasm.internal.externrefHashCode_1955433539' : 
+        'kotlin.wasm.internal.externrefToString_-951601593' : ref => String(ref),
+        'kotlin.wasm.internal.externrefEquals_1929080772' : (lhs, rhs) => lhs === rhs,
+        'kotlin.wasm.internal.externrefHashCode_-1529105245' : 
         (() => {
         const dataView = new DataView(new ArrayBuffer(8));
         function numberHashCode(obj) {
@@ -97,16 +100,82 @@ export async function instantiate(imports={}, runInitializer=true) {
             }
         }
         })(),
-        'kotlin.wasm.internal.isNullish_955678453' : (ref) => ref == null,
-        'kotlin.wasm.internal.tryGetOrSetExternrefBox_$external_fun_903163830' : (p0, p1) => tryGetOrSetExternrefBox(p0, p1),
-        'kotlin.js.__callJsClosure_o0vpnl_-332838811' : (f, p0) => f(p0),
-        'kotlin.js.__callJsClosure_vhne0b_-1225296359' : (f, p0) => f(p0),
-        'kotlin.random.initialSeed_714621149' : () => ((Math.random() * Math.pow(2, 32)) | 0),
-        'kotlinx.browser.document_$external_prop_getter_85945489' : () => document,
-        'org.w3c.dom.body_$external_prop_getter_-1797460166' : (_this) => _this.body,
-        'org.w3c.dom.createTextNode_$external_fun_1649979942' : (_this, p0) => _this.createTextNode(p0),
-        'org.w3c.dom.ownerDocument_$external_prop_getter_1104418837' : (_this) => _this.ownerDocument,
-        'org.w3c.dom.appendChild_$external_fun_-1637123743' : (_this, p0) => _this.appendChild(p0)
+        'kotlin.wasm.internal.isNullish_1738527798' : (ref) => ref == null,
+        'kotlin.wasm.internal.tryGetOrSetExternrefBox_$external_fun_64599960' : (p0, p1) => tryGetOrSetExternrefBox(p0, p1),
+        'kotlin.js.dynamicGetAny_881217544' : (obj, index) => obj[index],
+        'kotlin.io.printlnImpl_-1030976236' : (message) => console.log(message),
+        'kotlin.js.__callJsClosure_vjdw83_-1595539616' : (f, p0) => f(p0),
+        'kotlin.js.__callJsClosure_jh6aw8_2095731501' : (f, p0) => f(p0),
+        'kotlin.js.__convertKotlinClosureToJsClosure_wa1ql8_-1506129926' : (f) => () => wasmExports.__callFunction_wa1ql8(f, ),
+        'kotlin.random.initialSeed_-1139101152' : () => ((Math.random() * Math.pow(2, 32)) | 0),
+        'kotlinx.browser.window_$external_prop_getter_-825038626' : () => window,
+        'kotlinx.browser.document_$external_prop_getter_-106098477' : () => document,
+        'org.w3c.dom.length_$external_prop_getter_-1872258745' : (_this) => _this.length,
+        'org.w3c.dom.clipboard.__convertKotlinClosureToJsClosure_4t1ncd_-442670194' : (f) => (p0) => wasmExports.__callFunction_4t1ncd(f, p0),
+        'org.w3c.dom.css.setProperty_$external_fun_1734298686' : (_this, p0, p1, p2, isDefault0) => _this.setProperty(p0, p1, isDefault0 ? undefined : p2, ),
+        'org.w3c.dom.css.style_$external_prop_getter_587302038' : (_this) => _this.style,
+        'org.w3c.dom.events.addEventListener_$external_fun_1747931580' : (_this, p0, p1, p2, isDefault0) => _this.addEventListener(p0, p1, isDefault0 ? undefined : p2, ),
+        'org.w3c.dom.events.removeEventListener_$external_fun_1762166175' : (_this, p0, p1, p2, isDefault0) => _this.removeEventListener(p0, p1, isDefault0 ? undefined : p2, ),
+        'org.w3c.dom.events.type_$external_prop_getter_1452333404' : (_this) => _this.type,
+        'org.w3c.dom.events.target_$external_prop_getter_376780965' : (_this) => _this.target,
+        'org.w3c.dom.events.currentTarget_$external_prop_getter_-1048490452' : (_this) => _this.currentTarget,
+        'org.w3c.dom.events.eventPhase_$external_prop_getter_-1245958176' : (_this) => _this.eventPhase,
+        'org.w3c.dom.events.bubbles_$external_prop_getter_419664161' : (_this) => _this.bubbles,
+        'org.w3c.dom.events.cancelable_$external_prop_getter_2074147732' : (_this) => _this.cancelable,
+        'org.w3c.dom.events.defaultPrevented_$external_prop_getter_1109341682' : (_this) => _this.defaultPrevented,
+        'org.w3c.dom.events.composed_$external_prop_getter_1973664406' : (_this) => _this.composed,
+        'org.w3c.dom.events.isTrusted_$external_prop_getter_-1858347429' : (_this) => _this.isTrusted,
+        'org.w3c.dom.events.timeStamp_$external_prop_getter_-592242199' : (_this) => _this.timeStamp,
+        'org.w3c.dom.events.Event_$external_class_instanceof_1405976461' : (x) => x instanceof Event,
+        'org.w3c.dom.events.screenX_$external_prop_getter_-1431335696' : (_this) => _this.screenX,
+        'org.w3c.dom.events.screenY_$external_prop_getter_-1686072241' : (_this) => _this.screenY,
+        'org.w3c.dom.events.clientX_$external_prop_getter_2038959055' : (_this) => _this.clientX,
+        'org.w3c.dom.events.clientY_$external_prop_getter_1784222510' : (_this) => _this.clientY,
+        'org.w3c.dom.events.ctrlKey_$external_prop_getter_-228748081' : (_this) => _this.ctrlKey,
+        'org.w3c.dom.events.shiftKey_$external_prop_getter_874036390' : (_this) => _this.shiftKey,
+        'org.w3c.dom.events.altKey_$external_prop_getter_1849068429' : (_this) => _this.altKey,
+        'org.w3c.dom.events.metaKey_$external_prop_getter_542580457' : (_this) => _this.metaKey,
+        'org.w3c.dom.events.button_$external_prop_getter_1865140074' : (_this) => _this.button,
+        'org.w3c.dom.events.buttons_$external_prop_getter_730691003' : (_this) => _this.buttons,
+        'org.w3c.dom.events.relatedTarget_$external_prop_getter_1300868117' : (_this) => _this.relatedTarget,
+        'org.w3c.dom.events.pageX_$external_prop_getter_-1845892687' : (_this) => _this.pageX,
+        'org.w3c.dom.events.pageY_$external_prop_getter_-2100629232' : (_this) => _this.pageY,
+        'org.w3c.dom.events.x_$external_prop_getter_-1731407998' : (_this) => _this.x,
+        'org.w3c.dom.events.y_$external_prop_getter_-1986144543' : (_this) => _this.y,
+        'org.w3c.dom.events.offsetX_$external_prop_getter_-947552843' : (_this) => _this.offsetX,
+        'org.w3c.dom.events.offsetY_$external_prop_getter_-1202289388' : (_this) => _this.offsetY,
+        'org.w3c.dom.requestAnimationFrame_$external_fun_-397503097' : (_this, p0) => _this.requestAnimationFrame(p0),
+        'org.w3c.dom.__convertKotlinClosureToJsClosure_e64igx_218911805' : (f) => (p0) => wasmExports.__callFunction_e64igx(f, p0),
+        'org.w3c.dom.HTMLElement_$external_class_instanceof_1141286243' : (x) => x instanceof HTMLElement,
+        'org.w3c.dom.add_$external_fun_124764036' : (_this, p0) => _this.add(...p0),
+        'org.w3c.dom.classList_$external_prop_getter_483424870' : (_this) => _this.classList,
+        'org.w3c.dom.setAttribute_$external_fun_-10099328' : (_this, p0, p1) => _this.setAttribute(p0, p1),
+        'org.w3c.dom.removeAttribute_$external_fun_-1046638991' : (_this, p0) => _this.removeAttribute(p0),
+        'org.w3c.dom.Element_$external_class_instanceof_861511534' : (x) => x instanceof Element,
+        'org.w3c.dom.createElement_$external_fun_315804656' : (_this, p0, p1, isDefault0) => _this.createElement(p0, isDefault0 ? undefined : p1, ),
+        'org.w3c.dom.createTextNode_$external_fun_783836905' : (_this, p0) => _this.createTextNode(p0),
+        'org.w3c.dom.getElementById_$external_fun_303033340' : (_this, p0) => _this.getElementById(p0),
+        'org.w3c.dom.childNodes_$external_prop_getter_-982569715' : (_this) => _this.childNodes,
+        'org.w3c.dom.cloneNode_$external_fun_1003356427' : (_this, p0, isDefault0) => _this.cloneNode(isDefault0 ? undefined : p0, ),
+        'org.w3c.dom.insertBefore_$external_fun_56083325' : (_this, p0, p1) => _this.insertBefore(p0, p1),
+        'org.w3c.dom.appendChild_$external_fun_1819279683' : (_this, p0) => _this.appendChild(p0),
+        'org.w3c.dom.removeChild_$external_fun_-1356133203' : (_this, p0) => _this.removeChild(p0),
+        'org.w3c.dom.Text_$external_class_instanceof_1058512351' : (x) => x instanceof Text,
+        'org.w3c.dom.data_$external_prop_setter_1909535180' : (_this, v) => _this.data = v,
+        'org.w3c.dom.svg.SVGElement_$external_class_instanceof_690839412' : (x) => x instanceof SVGElement,
+        'kotlinx.coroutines.setTimeout_$external_fun_936906613' : (p0, p1) => setTimeout(p0, p1),
+        'org.jetbrains.compose.web.internal.runtime.jsCreateEventListener_2043309049' : handleEvent => ({ handleEvent }),
+        'org.jetbrains.compose.web.movementX_$external_prop_getter_-983443098' : (_this) => _this.movementX,
+        'org.jetbrains.compose.web.movementY_$external_prop_getter_-1238179643' : (_this) => _this.movementY,
+        'org.jetbrains.compose.web.dom.removeNeededAttrs_-752452336' :  (node) => {
+                    const names = node.getAttributeNames()
+                    for (const n of names) {
+                        if (n != 'style' && n != 'class') {
+                            node.removeAttribute(n);
+                        }
+                    }
+            }
+        
     }
     
     // Placed here to give access to it from externals (js_code)
